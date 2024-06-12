f6873e5216ec30b4688ec53cb78f0a784846ba11
gggxbbb <2331490629@qq.com>
2024-06-12 03:05:04 +0000

    allow public visit

--- web/app.js
+++ web/app.js
@@ -2161,6 +2161,8 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
     "null",
     "http://mozilla.github.io",
     "https://mozilla.github.io",
+    "http://pdf.gxb.icu",
+    "https://pdf.gxb.icu"
   ];
   // eslint-disable-next-line no-var
   var validateFileURL = function (file) {
@@ -2178,7 +2180,7 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
       // start accepting URLs from foreign origin -- CORS headers on the remote
       // server must be properly configured.
       if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
+        //throw new Error("file origin does not match viewer's");
       }
     } catch (ex) {
       PDFViewerApplication._documentError("pdfjs-loading-error", {
